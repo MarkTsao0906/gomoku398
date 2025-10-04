26a4683a86ecc7415d4af9145ce9d387abb9228f
MarkTsao0906 <marktsao0906@gmail.com>
2025-10-04 09:39:33 +0800

    更新：加了棋子聲音和標記功能

---
+++ public/client.js
@@ -0,0 +1,107 @@
+const socket = io();
+const canvas = document.getElementById("board");
+const ctx = canvas.getContext("2d");
+const size = 15;
+const cellSize = canvas.width / size;
+let board = Array(size).fill().map(() => Array(size).fill(null));
+let myTurn = false;
+let myPlayer = null;
+let roomId = null;
+let lastMove = null;
+
+// 棋盤
+function drawBoard() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = "#000";
+  for (let i = 0; i < size; i++) {
+    ctx.beginPath();
+    ctx.moveTo(i * cellSize + cellSize / 2, cellSize / 2);
+    ctx.lineTo(i * cellSize + cellSize / 2, canvas.height - cellSize / 2);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(cellSize / 2, i * cellSize + cellSize / 2);
+    ctx.lineTo(canvas.width - cellSize / 2, i * cellSize + cellSize / 2);
+    ctx.stroke();
+  }
+
+  // 棋子
+  for (let x = 0; x < size; x++) {
+    for (let y = 0; y < size; y++) {
+      if (board[x][y]) {
+        ctx.beginPath();
+        ctx.arc(
+          x * cellSize + cellSize / 2,
+          y * cellSize + cellSize / 2,
+          cellSize / 2.5,
+          0,
+          Math.PI * 2
+        );
+        ctx.fillStyle = board[x][y] === "black" ? "black" : "white";
+        ctx.fill();
+        ctx.stroke();
+
+        // 標記最後一步
+        if (lastMove && lastMove.x === x && lastMove.y === y) {
+          ctx.strokeStyle = "red";
+          ctx.strokeRect(
+            x * cellSize,
+            y * cellSize,
+            cellSize,
+            cellSize
+          );
+          ctx.strokeStyle = "#000";
+        }
+      }
+    }
+  }
+}
+
+drawBoard();
+
+// 落子
+canvas.addEventListener("click", (e) => {
+  if (!myTurn) return;
+  const x = Math.floor(e.offsetX / cellSize);
+  const y = Math.floor(e.offsetY / cellSize);
+  if (board[x][y]) return;
+
+  board[x][y] = myPlayer;
+  lastMove = { x, y };
+  drawBoard();
+  document.getElementById("moveSound").play();
+
+  socket.emit("makeMove", { roomId, x, y, player: myPlayer });
+  myTurn = false;
+});
+
+// 接收落子
+socket.on("moveMade", ({ x, y, player }) => {
+  board[x][y] = player;
+  lastMove = { x, y };
+  drawBoard();
+  document.getElementById("moveSound").play();
+  myTurn = (player !== myPlayer);
+});
+
+function joinRoom() {
+  roomId = document.getElementById("roomId").value;
+  if (!roomId) return alert("刷房卡");
+  socket.emit("joinRoom", roomId);
+}
+
+socket.on("roomJoined", (count) => {
+  if (count === 1) {
+    myPlayer = "black";
+    myTurn = true;
+    alert("你先手！");
+  } else if (count === 2) {
+    myPlayer = "white";
+    myTurn = false;
+    alert("你後手！");
+  }
+});
+
+socket.on("roomFull", () => {
+  alert("房間滿了！");
+});
--- server.js
+++ server.js
@@ -2,30 +2,45 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const path = require("path");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection", (socket) => {
-    console.log("一位玩家連線");
-
-    socket.on("joinRoom", (room) => {
-        socket.join(room);
-        socket.room = room;
-        console.log(`玩家加入房間: ${room}`);
-        socket.emit("joinedRoom", room);
-    });
-
-    socket.on("placePiece", (data) => {
-        // 廣播給同房間的其他玩家
-        socket.to(socket.room).emit("opponentMove", data);
-    });
-
-    socket.on("disconnect", () => {
-        console.log("一位玩家離線");
-    });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-    console.log(`伺服器運行在 http://localhost:${PORT}`);
+let rooms = {};
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("joinRoom", (roomId) => {
+    socket.join(roomId);
+    if (!rooms[roomId]) rooms[roomId] = [];
+    rooms[roomId].push(socket.id);
+
+    if (rooms[roomId].length > 2) {
+      socket.emit("roomFull");
+      socket.leave(roomId);
+    } else {
+      socket.emit("roomJoined", rooms[roomId].length);
+    }
+  });
+
+  socket.on("makeMove", ({ roomId, x, y, player }) => {
+    io.to(roomId).emit("moveMade", { x, y, player });
+  });
+
+  socket.on("disconnect", () => {
+    for (let room in rooms) {
+      rooms[room] = rooms[room].filter((id) => id !== socket.id);
+      if (rooms[room].length === 0) delete rooms[room];
+    }
+    console.log("user disconnected");
+  });
+});
+
+http.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
 });
